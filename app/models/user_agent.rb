967498ca0704acbb6c3b5b0c3c04a6c8d677ac60
Morgan Slimak <morganslimak@gmail.com>
2016-10-12 11:27:58 -0600

    finish user_agent calcs

--- app/models/user_agent.rb
+++ app/models/user_agent.rb
@@ -1,26 +1,20 @@
 class UserAgent < ActiveRecord::Base
   validates :user_agent, presence: true
   has_many :payloads
-  attr_accessor :os, :browser
-
-  def self.parse_browser(user_agent)
-    parsed = UserAgentParser.parse user_agent
-    @browser = parsed.to_s
-  end
 
   def self.list_browsers
     grouped = Payload.group(:user_agent).count
     grouped.map do |k, v|
-      parse_browser(k.user_agent)
-      [@browser, v]
+      parsed = UserAgentParser.parse k.user_agent
+      [parsed.to_s, v]
     end.to_h
   end
 
   def self.list_os
     grouped = Payload.group(:user_agent).count
     grouped.map do |k, v|
-      user_agent = UserAgentParser.parse k.user_agent
-      [user_agent.os.to_s, v]
+      parsed = UserAgentParser.parse k.user_agent
+      [parsed.os.to_s, v]
     end.to_h
   end
 end
