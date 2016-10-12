96a77577e148f9c6eeea3eca263af03120548c0f
Morgan Slimak <morganslimak@gmail.com>
2016-10-11 18:25:46 -0600

    add list_resolution method

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,4 +1,12 @@
 class Resolution < ActiveRecord::Base
   validates :resolution_width, :resolution_height, presence: true
   has_many :payloads
+
+  def self.list_resolutions
+    grouped = Payload.group(:resolution).count
+    grouped.map do |k, v|
+      [k.resolution_width + "x" + k.resolution_height, v]
+    end.to_h
+  end
+
 end
--- spec/models/resolution_spec.rb
+++ spec/models/resolution_spec.rb
@@ -19,4 +19,46 @@ RSpec.describe "resolution" do
     expect(resolution).to_not be_valid
   end
 
+  describe ".list_resolutions" do
+    it "lists all screen resolutions grouped by count" do
+      res1 = Resolution.create(resolution_width: "1920",
+                               resolution_height: "1080")
+      res2 = Resolution.create(resolution_width: "320",
+                               resolution_height: "568")
+
+      Payload.create({"url_id":1,
+                      "requested_at":"2013-02-16 21:38:28 -0700",
+                      "responded_in":37,
+                      "referred_by_id":1,
+                      "request_type_id":1,
+                      "event_name_id":1,
+                      "user_agent_id":1,
+                      "resolution_id":res1.id,
+                      "ip_id":1
+                    })
+      Payload.create({"url_id":1,
+                      "requested_at":"2013-02-16 21:38:28 -0700",
+                      "responded_in":37,
+                      "referred_by_id":1,
+                      "request_type_id":1,
+                      "event_name_id":1,
+                      "user_agent_id":1,
+                      "resolution_id":res1.id,
+                      "ip_id":1
+                    })
+      Payload.create({"url_id":1,
+                      "requested_at":"2013-02-16 21:38:28 -0700",
+                      "responded_in":37,
+                      "referred_by_id":1,
+                      "request_type_id":1,
+                      "event_name_id":1,
+                      "user_agent_id":1,
+                      "resolution_id":res2.id,
+                      "ip_id":1
+                    })
+      expected = {"1920x1080" => 2, "320x568" => 1}
+      expect(Resolution.list_resolutions).to eq(expected)
+    end
+  end
+
 end
